6625919f9b72e654c4274d7838a42b95fecd2782
drenisb <drenis.bajrami@student.uni-pr.edu>
2026-05-31 11:02:13 +0200

    Fxml Pages (no functionality)

---
+++ poker-client/src/main/java/pokergame/client/view/controllers/ConfigurationController.java
@@ -0,0 +1,14 @@
+package pokergame.client.view.controllers;
+
+import javafx.event.ActionEvent;
+
+public class ConfigurationController {
+
+    public void handleStartGame(ActionEvent actionEvent) {
+
+    }
+
+    public void handleStartMultiplayer(ActionEvent actionEvent) {
+
+    }
+}
